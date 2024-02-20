54e6696c4364608f35a8d23b3d14c21b0d9d02cb
Alex <garnettalexanderl@gmail.com>
2024-02-20 10:52:05 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,162 @@
+from flask import Flask, render_template, Response
+from process import *
+import cv2
+import platform
+import pytesseract.pytesseract
+import os
+import datefinder
+
+# Tell Tesseract-OCR where to find its training data
+os.environ['TESSDATA_PREFIX'] = 'Tesseract-OCR/tessdata'
+
+# Initialize some globals for use by webcam functions
+raw_image = None
+cam = None
+cam_on = False
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/capture')
+def stream_page():
+    return render_template('stream.html')
+
+
+def generate():
+    global raw_image
+    global cam
+    global cam_on
+    cam_on = True
+    cam = cv2.VideoCapture(0)
+    while cam_on:
+        # Continuously grab frames, convert to JPEG then bytes
+        success, raw_image = cam.read()
+        flag, output_frame = cv2.imencode('.jpg', raw_image)
+        if not flag:
+            continue
+        # todo: do edge detection on the ID
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+               bytearray(output_frame) + b'\r\n')
+
+
+@app.route('/camera_feed')
+def camera_feed():
+    # Return the image bytes with content type for the browser to read
+    return Response(generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
+@app.route('/display_capture')
+def display_capture():
+    # Convert last raw_image to JPEG bytes and returns content type for browser
+    flag, output_frame = cv2.imencode('.jpg', raw_image)
+    image_bytes = (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+                   bytearray(output_frame) + b'\r\n')
+    return Response(image_bytes, mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
+@app.route('/review_image')
+def review_image():
+    return render_template('review_image.html')
+
+
+def process_capture(image):
+    image = resize(image)
+    image = gray(image)
+    image = blur(image)
+    image = thresh(image)
+    image = morph(image)
+    return image
+
+
+def extract_data(image):
+    # Initialize the values we are hoping to extract
+    expiration = ""
+    first_name = ""
+    last_name = ""
+    issued = ""
+    address = ""
+
+    # I am developing on a windows laptop, but will deploy to linux server
+    if platform.system() == 'Windows':
+        pytesseract.pytesseract.tesseract_cmd = r'Tesseract-OCR\tesseract.exe'
+    cv2.imwrite("processed_capture.jpg", image)
+
+    # Get the text from the image and split it into lines
+    text = pytesseract.pytesseract.image_to_string(image)
+    _lines = text.split("\n")
+    print(_lines)
+
+    # Filter out blank lines
+    lines = []
+    for line in _lines:
+        if line is not '':
+            lines.append(line)
+
+    exp_index = 0
+    for line in lines:
+        matches = list(datefinder.find_dates(line))
+        if len(matches) > 0:
+            expiration = matches[0]
+            break
+        else:
+            exp_index += 1
+    print(exp_index)
+
+    ln_line = lines[exp_index + 1]
+    for char in ln_line:
+        if char.isalpha() and char.isupper():
+            last_name += char
+
+    fn_line = lines[exp_index + 2]
+    for char in fn_line:
+        if char.isalpha() and char.isupper():
+            first_name += char
+
+    address = lines[exp_index + 3] + " " + lines[exp_index + 4]
+
+    iss_line = lines[-1]
+    issued = iss_line.split(' ')[-1]
+    #     key = line[:3]
+    #     if key == "exp":
+    #         try:
+    #             expiration = dateutil.parser.parse(line)
+    #         except:
+    #             print("Expiration parsing failed. Input: " + line)
+    #     elif key == "ln ":
+    #         last_name = line[3:]
+    #     elif key == "fn ":
+    #         first_name = line[3:]
+    #
+    # last_line_split = lines[-1].split(' ')
+    # issued = last_line_split[-1]
+
+    print(lines)
+
+    results = {
+        'name': first_name + " " + last_name,
+        'address': address,
+        'issuance': issued,
+        'expiration': expiration,
+    }
+    return results, lines
+
+
+@app.route('/display_results')
+def display_results():
+    global cam
+    global cam_on
+    cam_on = False
+    cam.release()
+    cv2.imwrite('capture.jpg', raw_image)
+    processed_image = process_capture(raw_image)
+    results, lines = extract_data(processed_image)
+    return render_template("results.html", results=results)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80, debug=True)
---
+++ process.py
@@ -0,0 +1,143 @@
+import cv2
+import pytesseract
+import os
+import numpy as np
+import imutils
+from PIL import Image
+
+# https://pyimagesearch.com/2021/11/22/improving-ocr-results-with-basic-image-processing/
+
+pytesseract.pytesseract.tesseract_cmd = r'Tesseract-OCR\tesseract.exe'
+os.environ['TESSDATA_PREFIX'] = 'Tesseract-OCR/tessdata'
+
+file = {
+    "name": "test.jpg",
+    "data": "data",
+    "text": "text",
+    "keys_correct": "keys",
+    "values_correct": "values"
+}
+
+
+def extract_text(image):
+    text = pytesseract.pytesseract.image_to_string(image)
+    return text
+
+
+def evaluate(text):
+    keys_correct = 0
+    keys = ["EXP", "LN", "FN", "DOB"]
+    values_correct = 0
+    values = ["06/27/2024", "GARNETT", "ALEXANDER LEE", "06/27/1995"]
+    lines = text.split('\n')
+    print(lines)
+    for line in lines:
+        for key in keys:
+            if key in line:
+                keys_correct += 1
+        for value in values:
+            if value in line:
+                values_correct += 1
+    return "Keys correct: " + str(keys_correct) + "  Values correct: " + str(values_correct)
+
+
+def normalize(image):
+    norm_img = np.zeros((image.shape[0], image.shape[1]))
+    img = cv2.normalize(image, norm_img, 0, 255, cv2.NORM_MINMAX)
+    return img
+
+
+def deskew(image):
+    co_ords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(co_ords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC,
+                             borderMode=cv2.BORDER_REPLICATE)
+    return rotated
+
+
+def thresh(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+
+def gray(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+def blur(image):
+    height, width = image.shape[:2]
+    if height >= 720 or width >= 720:
+        image = cv2.GaussianBlur(image, (1, 1), 0)
+        image = cv2.bilateralFilter(image, 11, 41, 21)
+    else:
+        image = cv2.GaussianBlur(image, (1, 1), 0)
+        image = cv2.bilateralFilter(image, 11, 21, 7)
+    return image
+
+
+def distance(image):
+    image = cv2.distanceTransform(image, cv2.DIST_L2, 5)
+    image = cv2.normalize(image, None, 0, 1.0, cv2.NORM_MINMAX)
+    image = (image * 255).astype("uint8")
+    return image
+
+
+def morph(image):
+    height, width = image.shape[:2]
+    if height >= 720 or width >= 720:
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+        image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+    else:
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
+        image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+    return image
+
+
+def resize(image):
+    height, width = image.shape[:2]
+    max_height = 720
+    max_width = 1080
+    if width < 1080 or height < 720:
+        pass
+        # raise Exception(f"This application does not support images of resolution "
+        #                 f"less than 1080x720. Your image is {width}x{height}.")
+    elif width > max_width or height > max_height:
+        scaling_factor = max_height/float(height)
+        if max_width/float(width) < scaling_factor:
+            scaling_factor = max_width/float(width)
+        image = cv2.resize(image, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
+    return image
+
+
+def contours(image):
+    cnts = cv2.findContours(image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    chars = []
+
+    for c in cnts:
+        (x, y, w, h) = cv2.boundingRect(c)
+        # if w >= 35 and h >= 100:
+        chars.append(c)
+
+        # compute the convex hull of the characters
+        chars = np.vstack([chars[i] for i in range(0, len(chars))])
+        hull = cv2.convexHull(chars)
+        # allocate memory for the convex hull mask, draw the convex hull on
+        # the image, and then enlarge it via a dilation
+        mask = np.zeros(image.shape[:2], dtype="uint8")
+        cv2.drawContours(mask, [hull], -1, 255, -1)
+        mask = cv2.dilate(mask, None, iterations=2)
+        # take the bitwise of the opening image and the mask to reveal *just*
+        # the characters in the image
+        final = cv2.bitwise_and(image, image, mask=mask)
+        return final
+
+
