23c4e68aba7f06de663ee9439efd947b9ec5f641
Alex <garnettalexanderl@gmail.com>
2024-02-20 15:39:59 -0800

    More script to try to enable camera

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,5 @@
+let streamStarted = false;
+
 const constraints = {
   video: {
     width: {
@@ -9,10 +11,26 @@ const constraints = {
   }
 };
 
+const getCameraSelection = async () => {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter(device => device.kind === 'videoinput');
+  const options = videoDevices.map(videoDevice => {
+    return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
+  });
+  cameraOptions.innerHTML = options.join('');
+};
+
 const startStream = async (constraints) => {
   const stream = await navigator.mediaDevices.getUserMedia(constraints);
   handleStream(stream);
 };
+
+const handleStream = (stream) => {
+  video.srcObject = stream;
+  streamStarted = true;
+};
+
+getCameraSelection();
 
 startStream(constraints);
 
